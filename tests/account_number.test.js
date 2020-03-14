16e004dd0728535e8b25191cedb9c3bbb91e7219
Van Anderson <van.m.anderson@gmail.com>
2020-03-14 18:10:59 -0500

    add checksum calculations to account number class, clean up some cruft

--- tests/account_number.test.js
+++ tests/account_number.test.js
@@ -30,5 +30,41 @@ describe(('AccountNumber'), () => {
     const accountNumber = new AccountNumber(scannedDigits)
       
     equal(123456789, accountNumber.toInteger());
-  }); 
+  });
+  
+  it('passes checksum when given valid account digits', ()=>{
+    const scannedDigits = [ 
+      new ScannedDigit(one),
+      new ScannedDigit(two),
+      new ScannedDigit(three),
+      new ScannedDigit(four),
+      new ScannedDigit(five),
+      new ScannedDigit(six),
+      new ScannedDigit(seven),
+      new ScannedDigit(eight),
+      new ScannedDigit(nine)
+    ];
+
+    const accountNumber = new AccountNumber(scannedDigits)
+      
+    equal(true, accountNumber.isChecksumValid());
+  });
+
+  it('fails checksum when given invalid account digits', ()=>{
+    const scannedDigits = [ 
+      new ScannedDigit(two),
+      new ScannedDigit(two),
+      new ScannedDigit(three),
+      new ScannedDigit(four),
+      new ScannedDigit(five),
+      new ScannedDigit(six),
+      new ScannedDigit(seven),
+      new ScannedDigit(eight),
+      new ScannedDigit(nine)
+    ];
+
+    const accountNumber = new AccountNumber(scannedDigits)
+      
+    equal(false, accountNumber.isChecksumValid());
+  })
 });
