3e1226ea4a8057df4fb88a70103cfe9a62cfc704
Van Anderson <van.m.anderson@gmail.com>
2020-03-14 15:50:09 -0500

    setup vscode debugger tools, set up first failing spec

--- tests/scanned_digit.test.js
+++ tests/scanned_digit.test.js
@@ -1,7 +1,11 @@
-import ScannedDigit from '../lib/greeter';
+import ScannedDigit from '../lib/scanned-digit';
+import { one } from '../lib/scanned-digit/digits';
 import { equal } from "assert";
 
 
 describe(('ScannedDigit'), () => {
-  it('will do something', () => {})
+  it('will read the correct value', () => {
+    const digit = new ScannedDigit(one)
+    equal(1, digit.toInteger);
+  })
 })
