3721b275a2407f86396669a56bf65a193aea8c09
Van Anderson <van.m.anderson@gmail.com>
2020-03-14 16:14:12 -0500

    AccountNumber class consumes ScannedDigits and maps them to either a string or a integer - specs to confirm functionality

---
+++ tests/account_number.test.js
@@ -0,0 +1,34 @@
+import AccountNumber from '../lib/account-number';
+import ScannedDigit from '../lib/scanned-digit';
+
+import { one, 
+         two, 
+         three,
+         four,
+         five,
+         six, 
+         seven, 
+         eight, 
+         nine } from '../lib/scanned-digit/digits';
+import { equal } from "assert";
+
+
+describe(('AccountNumber'), () => {
+  it('translate a group of 9 ScannedDigits to an integer', () => {
+    const scannedDigits = [ 
+      new ScannedDigit(one),
+      new ScannedDigit(two),
+      new ScannedDigit(three),
+      new ScannedDigit(four),
+      new ScannedDigit(five),
+      new ScannedDigit(six),
+      new ScannedDigit(seven),
+      new ScannedDigit(eight),
+      new ScannedDigit(nine)
+    ];
+
+    const accountNumber = new AccountNumber(scannedDigits)
+      
+    equal(123456789, accountNumber.toInteger());
+  }); 
+});
