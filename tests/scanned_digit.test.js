0e7a7d4d2186a7c34504fe4480b489eba863bce1
Van Anderson <van.m.anderson@gmail.com>
2020-03-14 15:57:46 -0500

    continue spec pattern for other digits; fix mistake in the digits constants to make specs pass

--- tests/scanned_digit.test.js
+++ tests/scanned_digit.test.js
@@ -1,11 +1,59 @@
 import ScannedDigit from '../lib/scanned-digit';
-import { one } from '../lib/scanned-digit/digits';
+import { one, 
+         two, 
+         three,
+         four,
+         five,
+         six, 
+         seven, 
+         eight, 
+         nine } from '../lib/scanned-digit/digits';
 import { equal } from "assert";
 
 
 describe(('ScannedDigit'), () => {
-  it('will read the correct value', () => {
-    const digit = new ScannedDigit(one)
+  it('will read the correct value for one', () => {
+    const digit = new ScannedDigit(one);
     equal(1, digit.toInteger());
-  })
-})
+  });
+
+  it('will read the correct value for two', () => {
+    const digit = new ScannedDigit(two);
+    equal(2, digit.toInteger());
+  });
+
+  it('will read the correct value for three', () => {
+    const digit = new ScannedDigit(three);
+    equal(3, digit.toInteger());
+  });
+
+  it('will read the correct value for four', () => {
+    const digit = new ScannedDigit(four);
+    equal(4, digit.toInteger());
+  });
+
+  it('will read the correct value for five', () => {
+    const digit = new ScannedDigit(five);
+    equal(5, digit.toInteger());
+  });
+
+  it('will read the correct value for six', () => {
+    const digit = new ScannedDigit(six);
+    equal(6, digit.toInteger());
+  });
+
+  it('will read the correct value for seven', () => {
+    const digit = new ScannedDigit(seven);
+    equal(7, digit.toInteger());
+  });
+
+  it('will read the correct value for eight', () => {
+    const digit = new ScannedDigit(eight);
+    equal(8, digit.toInteger());
+  });
+
+  it('will read the correct value for nine', () => {
+    const digit = new ScannedDigit(nine);
+    equal(9, digit.toInteger());
+  });
+});
